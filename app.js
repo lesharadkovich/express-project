281e60ef36e6f401a603b2559baa6fde435fec30
alexey.radkovich <alexey.radkovich@gmail.com>
2017-10-11 11:27:14 +0300

    fix

--- app.js
+++ app.js
@@ -28,9 +28,9 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+// app.use('/', index);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
